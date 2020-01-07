6af3979e716a108617eb9244e3660049da6cf810
unknown <komalbhuvanagiri@gmail.com>
2020-01-07 18:10:17 -0500

    Updates

---
+++ precache-manifest.c9f9682058ee595f31e8b549ced8aae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7faca4be660c54e073ecab52962a482b",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "832960d21679a8ffd486",
+    "url": "/react/static/css/main.59ea4230.chunk.css"
+  },
+  {
+    "revision": "9ccc463a876c95ed82b4",
+    "url": "/react/static/js/2.7fb7e1f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react/static/js/2.7fb7e1f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "832960d21679a8ffd486",
+    "url": "/react/static/js/main.888015c2.chunk.js"
+  },
+  {
+    "revision": "b5c8d079e7da5f4e71af",
+    "url": "/react/static/js/runtime-main.65b51044.js"
+  }
+]);
